f865a0ed6565e6c68e95a30996e1d4d888ca0e1a
abansal2101 <abansal2107@gmail.com>
2021-05-15 09:53:42 +0530

    Updates

---
+++ precache-manifest.d6619f52c7894dfe91cea18963e2411c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0afad390c325cb13d741",
+    "url": "/ReduxExpoWeb/static/js/app.e33a24eb.chunk.js"
+  },
+  {
+    "revision": "7a03bac0c2315d0d9f65",
+    "url": "/ReduxExpoWeb/static/js/runtime~app.483b8671.js"
+  },
+  {
+    "revision": "14f658d5f3063952ab9b",
+    "url": "/ReduxExpoWeb/static/js/2.b6650ac3.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/ReduxExpoWeb/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/ReduxExpoWeb/expo-service-worker.js"
+  },
+  {
+    "revision": "a9ed990e5e2ac6c80210ab5315128c64",
+    "url": "/ReduxExpoWeb/register-service-worker.js"
+  },
+  {
+    "revision": "4ebf0be6877fb7d3dcda8bf7fe2abfba",
+    "url": "/ReduxExpoWeb/static/js/2.b6650ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/ReduxExpoWeb/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/ReduxExpoWeb/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/ReduxExpoWeb/favicon.ico"
+  },
+  {
+    "revision": "c2e52508173f10ce97988f9c7535b4b6",
+    "url": "/ReduxExpoWeb/index.html"
+  },
+  {
+    "revision": "d49780b24b8b85c1ca2742bc27d2e70f",
+    "url": "/ReduxExpoWeb/manifest.json"
+  }
+];
